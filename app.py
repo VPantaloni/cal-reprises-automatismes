4b1ea8413a074cae59315ee08ad756487ee84034
Vincent Pantaloni <iaiprvp@gmail.com>
2025-07-05 22:18:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -201,8 +201,6 @@ if st.sidebar.button("📙 Progression n°2"):
 
 top_button_placeholder = st.sidebar.empty()
 #---
-from collections import defaultdict
-from selection_q3 import selectionner_q3, reconstruire_auto_weeks
 
 if st.sidebar.button("▶️ Sélection Q1/Q2 uniquement"):
     auto_weeks = defaultdict(list)
@@ -217,6 +215,8 @@ if st.sidebar.button("▶️ Sélection Q1/Q2 uniquement"):
     st.rerun()
 
 if st.sidebar.button("🧩 Compléter Q3 ❓"):
+    from selection_q3 import selectionner_q3, reconstruire_auto_weeks
+
     auto_weeks, used_codes = reconstruire_auto_weeks(st.session_state.selection_by_week)
 
     st.session_state.selection_by_week = selectionner_q3(
